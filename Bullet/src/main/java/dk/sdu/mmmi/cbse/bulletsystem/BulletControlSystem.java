94faa5cf370ecd73387e47aa2e344485b2bd3f3e
ulhen <umh@outlook.dk>
2025-05-15 11:03:51 +0200

    Initial commit

---
+++ Bullet/src/main/java/dk/sdu/mmmi/cbse/bulletsystem/BulletControlSystem.java
@@ -0,0 +1,35 @@
+package dk.sdu.mmmi.cbse.bulletsystem;
+
+import dk.sdu.mmmi.cbse.common.bullet.Bullet;
+import dk.sdu.mmmi.cbse.common.bullet.BulletSPI;
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+
+public class BulletControlSystem implements IEntityProcessingService, BulletSPI {
+
+    @Override
+    public void process(GameData gameData, World world) {
+
+        for (Entity bullet : world.getEntities(Bullet.class)) {
+            double changeX = Math.cos(Math.toRadians(bullet.getRotation()));
+            double changeY = Math.sin(Math.toRadians(bullet.getRotation()));
+            bullet.setX(bullet.getX() + changeX * 3);
+            bullet.setY(bullet.getY() + changeY * 3);
+        }
+    }
+
+    @Override
+    public Entity createBullet(Entity shooter, GameData gameData) {
+        Entity bullet = new Bullet();
+        bullet.setPolygonCoordinates(1, -1, 1, 1, -1, 1, -1, -1);
+        double changeX = Math.cos(Math.toRadians(shooter.getRotation()));
+        double changeY = Math.sin(Math.toRadians(shooter.getRotation()));
+        bullet.setX(shooter.getX() + changeX * 10);
+        bullet.setY(shooter.getY() + changeY * 10);
+        bullet.setRotation(shooter.getRotation());
+        bullet.setRadius(1);
+        return bullet;
+    }
+}
