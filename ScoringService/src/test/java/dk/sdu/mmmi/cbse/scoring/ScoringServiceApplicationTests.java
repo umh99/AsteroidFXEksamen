f3617eb705c7f8e41314e3326dce8f377e568157
ulhen <umh@outlook.dk>
2025-05-26 12:06:26 +0200

    its beginning to workings

---
+++ ScoringService/src/test/java/dk/sdu/mmmi/cbse/scoring/ScoringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.sdu.mmmi.cbse.scoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
